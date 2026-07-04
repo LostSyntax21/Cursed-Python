a096877708f53deae16349e033d37725fec1b329
NicknamedTwice <bigb3n42@gmail.com>
2026-07-05 00:54:13 +0100

    feat: boolean logic has left the chat

--- 05_liar.py
+++ 05_liar.py
@@ -3,3 +3,4 @@ x = type('', (), {'__eq__': lambda *_: True, '__bool__': lambda *_: False})()
 print(x in [1, 2, 3])  # True
 print(x == 5 and x == 6)  # True
 print(bool(x) == (x == True))  # False
+
